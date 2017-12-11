9c74f9f3320b5ef088dd9cf5c0db58c91875c83b
Kusuma18 <kusumasv93@gmail.com>
2017-12-12 00:37:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,90 @@
+﻿(function (angular) {
+    'use strict';
+    angular.module('invoice', ['invoice2','chart.js'])
+      .controller('InvoiceController', ['currencyConverter' , function InvoiceController(currencyConverter) {
+          this.qty = 1;
+          this.cost = 2;
+          this.InCurr;
+          var k;
+          this.currencies = [];
+          var ratesGot = [];
+          var Ratesgot = function Ratesgot() {
+              var kk;
+              for (kk = 0; kk < currencyConverter.Rates.length; kk++) {
+
+                  ratesGot.push(currencyConverter.Rates[kk].rate);
+              }
+          };
+          this.currencyCalling = function currencyCalling() {
+              for (k = 0; k < currencyConverter.Currencies.length; k++) {
+
+                  this.currencies.push(currencyConverter.Currencies[k]);
+
+              };
+              debugger;
+              Ratesgot();
+          };
+          this.rates = function rates(abc) {
+              var i;
+              for (i = 0; i < currencyConverter.Rates.length; i++) {
+                  if (abc === currencyConverter.Rates[i].ID)
+                      return currencyConverter.Rates[i].rate;
+              }
+
+
+          };
+          
+          
+          
+          this.total = function total(outcurr) {
+              return currencyConverter.Convert(this.qty * this.cost, this.InCurr, outcurr);
+          };
+          
+          this.payAmount = function payAmount() {
+              window.alert("Thanks!");
+          };
+          
+          var labels;
+          var datasets;
+          var scales;
+          var legend;
+          var title;
+          var ctx = document.getElementById("myChart").getContext('2d');
+          var myChart = new Chart(ctx, {
+              type: 'bar',
+              data: {
+                  labels: this.currencies,
+                  datasets: [{
+                      label: 'Exchange Rates',
+                      data: ratesGot,
+                      backgroundColor: [
+                          'rgba(255, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(255, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(153, 102, 255, 0.2)',
+                          'rgba(255, 159, 64, 0.2)'
+                      ],
+                      borderColor: [
+                          'rgba(255,99,132,1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)'
+                      ],
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero: true
+                          }
+                      }]
+                  }
+              }
+          });
+      }]);
+})(window.angular);
---
+++ invoice2.js
@@ -0,0 +1,118 @@
+﻿(function (angular) {
+    'use strict';
+    angular.module('invoice2',[])
+    .factory('currencyConverter', ['$http', function ($http, $timeout) {
+        
+        var Currencies = [];
+        var Rates = [];
+        var usdToForeignRates = {};
+        
+        
+        //var usdtocon = {
+        //    'EUR': 1,
+        //    'USD': 2,
+        //    'INR': 3
+        //};
+        
+        
+        //var Convert = function (amount, InCurr, outcurr) {
+        //    //if (ID = '') {
+        //    //    return amount * t[outcurr] / idrates[InCurr].rate;
+        //    //}
+            
+        //    ////
+        //};
+        
+        var i;
+        var ID;
+        var rate;
+        var ratesjsonarray = [];
+        var refresh = function refresh() {
+            var url = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
+            debugger;
+            return $http.get(url).then(function (response) {
+                usdToForeignRates = response.data;//json
+                
+                var stringedjson = JSON.stringify(usdToForeignRates);//string
+                var t = JSON.parse(stringedjson);//array
+                
+                
+                    
+                for (i = 0; i < t.length; i++) {
+                    
+                    ratesjsonarray.push({
+                        ID: t[i].id,
+                        rate: t[i].percent_change_1h
+                    });
+
+                };
+                abc();
+                ced();
+
+                
+                
+            });
+                
+        };
+        var getData = function () {
+            var url = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
+            $http.get(url).then(function (response) {
+                usdToForeignRates = response.data;//json
+
+                var stringedjson = JSON.stringify(usdToForeignRates);//string
+                var t = JSON.parse(stringedjson);//array
+
+
+
+                for (i = 0; i < t.length; i++) {
+
+                    ratesjsonarray.push({
+                        ID: t[i].id,
+                        rate: t[i].percent_change_1h
+                    });
+
+                };
+                abc();
+                ced();
+
+
+
+            });
+        };
+
+        // Function to replicate setInterval using $timeout service.
+        var intervalFunction = function () {
+            $timeout(function () {
+                getData();
+                intervalFunction();
+            }, 300000)
+        };
+
+        // Kick off the interval
+        intervalFunction();
+        
+        var abc = function abc() {
+            var k;
+            for (k = 0; k < ratesjsonarray.length; k++) {
+                Currencies.push(ratesjsonarray[k].ID);
+            }
+        };
+        var ced = function ced() {
+            var k;
+            for (k = 0; k < ratesjsonarray.length; k++) {
+                Rates.push(ratesjsonarray[k].rate);
+            }
+        };
+        debugger;
+        //refresh();
+        
+        //this.oneh_rate = this.usdt.percent_change_1h;
+        
+        
+        return {
+            Currencies: Currencies,
+            Convert: Convert,
+            Rates: ratesjsonarray
+        };
+    }]);
+})(window.angular);
